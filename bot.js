a2a7271191d8e3143a9f25e05368d548aeaed900
mmmmoju <50597545+mmmmoju@users.noreply.github.com>
2019-05-15 11:58:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("578156819613220866")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`سبام`);
 }, 30)
 })
 
